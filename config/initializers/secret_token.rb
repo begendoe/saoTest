0702f4b5ac461f1ac0a7c36475805e273f878cd5
Brian Egendoerfer <begendoe@nd.edu>
2014-02-05 10:33:46 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoaTest::Application.config.secret_token = '4419b92e3d61fb4f74d0cf06c3f6790024776b1cadf4607495c684fdd3ea0a636cd87e3d2e1a399f85f0a22759a0052b2acf9084b621d8aa5212db4a49b73b31'
